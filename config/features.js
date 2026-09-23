c783985e673485b4d69a6af94a0bc5c0420f687f
zekainstabot <1999farpi@gmail.com>
2026-09-23 20:00:15 +0330

    Create features.js
    feat: add feature flags configuration

---
+++ config/features.js
@@ -0,0 +1,22 @@
+const features = {
+  instagram: true,
+
+  tiktok: false,
+  youtube: false,
+  facebook: false,
+  x: false,
+  pinterest: false,
+
+  watcher: false,
+  collections: false,
+  discovery: false,
+
+  referral: false,
+  missions: false,
+  luck: false,
+
+  payments: false,
+  support: true,
+};
+
+module.exports = features;
