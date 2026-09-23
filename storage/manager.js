7bf82324950a4e3b72213a20340d5bf04a2b715a
zekainstabot <1999farpi@gmail.com>
2026-09-23 19:49:15 +0330

    Create manager.js
    feat: add storage manager

---
+++ storage/manager.js
@@ -0,0 +1,10 @@
+/**
+ * Storage Manager
+ *
+ * مسئول مدیریت فایل‌های موقت دانلودهاست.
+ * منطق کامل Storage بعداً به این ماژول اضافه می‌شود.
+ */
+
+module.exports = {
+  name: "StorageManager",
+};
